edadbee8b2f363da7c75329b75600d06c9d954f6
Vignesh R <iamvigneshofficial@gmail.com>
2025-08-24 16:20:51 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/E-Commerce-Website-Baking-WebApplication-/',
-  plugins: [react()],
-})
